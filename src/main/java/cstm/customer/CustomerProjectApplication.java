c723d2fa88422ed6483f746d93e5e109c2c5fe15
semo41105 <semo41105@gmail.com>
2021-07-17 21:13:34 +0900

    initial commit
    기본 셋팅

---
+++ src/main/java/cstm/customer/CustomerProjectApplication.java
@@ -0,0 +1,13 @@
+package cstm.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProjectApplication.class, args);
+	}
+
+}
