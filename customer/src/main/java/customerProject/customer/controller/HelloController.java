6a8e5d9d4a65c1e3732630afece4ef54d6abe3cc
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-25 18:02:23 +0900

    bootstrap 적용

--- customer/src/main/java/customerProject/customer/controller/HelloController.java
+++ customer/src/main/java/customerProject/customer/controller/HelloController.java
@@ -1,15 +1,22 @@
 package customerProject.customer.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class HelloController {
 
-    @GetMapping("hello")
+/*    @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
+        return "index";
+    }*/
+
+    @GetMapping("/")
+    public String main(){
         return "index";
     }
 }
