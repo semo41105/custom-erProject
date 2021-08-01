404135fadcdcadefab7d9a2a1ae39f0bfa18aa53
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2021-08-01 14:32:27 +0900

    헤더, 메인페이지 일부 제작

--- customer/src/main/java/customerProject/customer/controller/MainController.java
+++ customer/src/main/java/customerProject/customer/controller/MainController.java
@@ -2,21 +2,15 @@ package customerProject.customer.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
-public class HelloController {
-
-/*    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "index";
-    }*/
+public class MainController {
 
     @GetMapping("/")
     public String main(){
         return "index";
     }
+
 }
