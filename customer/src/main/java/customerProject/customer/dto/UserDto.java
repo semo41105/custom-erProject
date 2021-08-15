a433e660a5c82632affeb3607a9e87cc19d78c3c
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2021-08-15 18:08:36 +0900

    Changes by somi

--- customer/src/main/java/customerProject/customer/dto/UserDto.java
+++ customer/src/main/java/customerProject/customer/dto/UserDto.java
@@ -13,7 +13,7 @@ public class UserDto {
 
     private String id;
     private String pw;
-    private String userName;
+    private String name;
     private String phone;
     private String email;
 
@@ -25,7 +25,7 @@ public class UserDto {
     public UserDto(String id, String pw, String name, String phone, String email, String city, String street, String zipcode) {
         this.id = id;
         this.pw = pw;
-        this.userName = userName;
+        this.name = name;
         this.phone = phone;
         this.email = email;
         this.city = city;
@@ -39,7 +39,7 @@ public class UserDto {
         return Member.builder()
                 .id(id)
                 .pw(pw)
-                .name(userName)
+                .name(name)
                 .phone(phone)
                 .email(email)
                 .address(address)
